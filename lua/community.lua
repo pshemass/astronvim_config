456b134066bede0411071189bb604f54978f3c61
pshemass <rzbikson@gmail.com>
2025-04-07 15:03:07 -0400

    remove copilot and metals overrides

--- lua/community.lua
+++ lua/community.lua
@@ -11,8 +11,8 @@ return {
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.helm" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.editing-support.copilotchat-nvim" },
+  --{ import = "astrocommunity.completion.copilot-lua-cmp" },
+  --{ import = "astrocommunity.editing-support.copilotchat-nvim" },
   { import = "astrocommunity.utility.lua-json5" },
   { import = "astrocommunity.test.neotest" },
 
